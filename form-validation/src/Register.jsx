3b9b530966146907514c608169dba9a861a74ae8
Harsh Patil <harshpatil223@gmail.com>
2023-03-22 16:44:00 +0530

    remove unused import

--- form-validation/src/Register.jsx
+++ form-validation/src/Register.jsx
@@ -5,7 +5,6 @@ import {
   faInfoCircle,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Link } from "react-router-dom";
 
 const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
